35b8e7706f47c677b6614b98b6dbb6b02b0d243f
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-07-10 14:54:29 -0700

    Add files via upload

--- Gae1.py
+++ Gae1.py
@@ -103,10 +103,15 @@ else:
         def create_pie_chart(df, title):
             rating_counts = df['Rating'].value_counts()
             plt.figure(figsize=(8, 8))
-            plt.pie(rating_counts, labels=rating_counts.index, autopct='%1.1f%%', startangle=140, colors=sns.color_palette('viridis'))
+            plt.pie(rating_counts, labels=rating_counts.index, autopct='%1.1f%%', startangle=140, colors=sns.color_palette('cividis'))
             plt.title(title, color='white')
             plt.gca().patch.set_facecolor('#303030')
             plt.gcf().set_facecolor('#303030')
+            for text in plt.gca().texts:
+                text.set_color('white')  # Set pie chart text color to white
+            # Adjust the color of the outer labels
+            for text in plt.gca().texts[-len(rating_counts):]:
+                text.set_color('white')
             st.pyplot(plt)
         
         st.write("### Rating Distribution for Selected Category")
